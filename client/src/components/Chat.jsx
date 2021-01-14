9f7b38250ea7b719ccb4522515f3e4a3e68db8e8
sohanr <mizan.rahman66d@gmail.com>
2021-01-15 01:19:55 +0600

    source

---
+++ client/src/components/Chat.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Chat = () => {
+    return(
+        <h1>helloooooooooooooo</h1>
+    )
+}
+
+export default Chat;
---
+++ client/src/components/Join.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Join = () => {
+    return(
+        <h1>join plz </h1>
+    )
+}
+
+export default Join;
