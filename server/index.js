8f0b0d088ea9ffe91be511790ae45bed49452623
sohanr <mizan.rahman66d@gmail.com>
2021-01-17 21:18:24 +0600

    try with cors

--- server/index.js
+++ server/index.js
@@ -1,26 +1,39 @@
 const express = require('express');
 const socketio = require('socket.io');
+
 const http = require('http');
 const router = require('./router/router');
 
-const PORT = process.env.PORT ||5000;
+const cors = require('cors');
+
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const options = {
+    cors: {
+        origin: "http://localhost:3000/",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["my-custom-header"],
+        credentials: true
+      }
+}
+const io = socketio(server, options);
 
 
-app.use(router);
+
 
 //socket.io
 
-io.on('connected', (socket) =>{
+io.on('connection', socket => {
     console.log("we have a new user!!!!!!!!!");
 
-    socket.on('disconnected', () =>{
+    socket.on('disconnect', () =>{
         console.log('User had left!');
     })
 })
 
+app.use(cors())
+app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on port:${PORT}`));
