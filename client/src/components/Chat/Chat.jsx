f95976eb9cd78ba5920156a5a0cac477afde5d71
sohanr <mizan.rahman66d@gmail.com>
2021-01-18 15:56:14 +0600

    cors fixed

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -2,22 +2,26 @@ import queryString from 'query-string';
 import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 
-let socket;
 
-const Chat = ({location}) => {
+
+const Chat = ({ location }) => {
 
     const [name, setName] = useState("");
     const [room, setRoom] = useState("");
     const EP = 'http://localhost:5000/';
-
+   
     useEffect(() =>{
         const {name , room} = queryString.parse(location.search);
 
-        socket = io(EP,{
+        const socket = io.connect(EP , {
+            "force new connection" : true,
+            "reconnectionAttempts": "Infinity", 
+            "timeout" : 10000,                  
+            "transports" : ["websocket"],
             withCredentials:true,
-            extraHeaders:{
-                "my-custom-header": "abcd"
-            }
+                extraHeaders:{
+                    "my-custom-header": "abcd"
+                }
         });
 
         setName(name);
