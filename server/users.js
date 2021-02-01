e75696bddddfb04e29b8153412ed26e78127f2a3
sohanr <mizan.rahman66d@gmail.com>
2021-02-01 16:48:56 +0600

    correct error

--- server/users.js
+++ server/users.js
@@ -7,7 +7,7 @@ const addUser = ({id, name , room}) =>{
     const existingUser = users.find((user) => user.room === room && user.name === name)
 
     if(existingUser){
-        return {erro: 'Username is taken'}
+        return {error: 'Username is taken'}
     }
 
     const user = {id, name, room}
