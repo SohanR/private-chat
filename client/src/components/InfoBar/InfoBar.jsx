16b32d83431893b5d8b5b884cfc5a654f090e810
sohanr <mizan.rahman66d@gmail.com>
2021-02-02 20:27:32 +0600

    infobar component

--- client/src/components/InfoBar/InfoBar.jsx
+++ client/src/components/InfoBar/InfoBar.jsx
@@ -5,20 +5,16 @@ import './InfoBar.css';
 
 
 
-const InfoBar = ({room}) => (
-    
-    <div className='infoBar'>
-        <div className='leftInnerContainer' >
-            <img className='onlineIcon' src={onlineIcon} alt="onlineIcon" />
-
-            <h3>{room}</h3>
-        </div>
-
-        <div className='rightInnerContainer' >
-            <a href="/"> <img src={closeIcon} alt="closeIcon"/> </a>
-        </div>
+const InfoBar = ({ room }) => (
+  <div className="infoBar">
+    <div className="leftInnerContainer">
+      <img className="onlineIcon" src={onlineIcon} alt="online icon" />
+      <h3>{room}</h3>
     </div>
-    
-)
+    <div className="rightInnerContainer">
+      <a href="/"><img src={closeIcon} alt="close icon" /></a>
+    </div>
+  </div>
+);
 
 export default InfoBar;
