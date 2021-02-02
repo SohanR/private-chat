22f994e96e54c0864e6b7b15340ac7cdea87ba98
sohanr <mizan.rahman66d@gmail.com>
2021-02-02 20:28:47 +0600

    iput comp

--- client/src/components/Input/Input.jsx
+++ client/src/components/Input/Input.jsx
@@ -2,22 +2,18 @@ import React from 'react';
 import './Input.css';
 
 
-
-
-const Input = ({message , setMessage, sendMassage}) => (    
-    
-    <form className='form' >
-        <input 
-            className='input'
-            type="text" 
-            placeholder='Type a private message..'
-            value={message}
-            onChange={(event) => setMessage(event.target.value)}
-            onKeyPress={(event) => event.key === 'Enter' ? sendMassage(event) : null}        
-        />
-        <button className='sendButton' onClick={(event) => sendMassage(event)} >Send</button>
-    </form>
-    
+const Input = ({ setMessage, sendMessage, message }) => (
+  <form className="form">
+    <input
+      className="input"
+      type="text"
+      placeholder="Type a message..."
+      value={message}
+      onChange={({ target: { value } }) => setMessage(value)}
+      onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
+    />
+    <button className="sendButton" onClick={e => sendMessage(e)}>Send</button>
+  </form>
 )
 
 export default Input;
